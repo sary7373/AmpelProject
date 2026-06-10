9e2bbaab4c75450c0b576da24537d64068bc19bc
Sarah <sarahchristen74@gmail.com>
2026-06-10 09:39:19 +0200

    green red cycle

--- project.py
+++ project.py
@@ -3,32 +3,54 @@ from machine import Pin
 
 led = Pin("LED", Pin.OUT)
 
-a1_rot = Pin(6, Pin.OUT)
-a1_gelb = Pin(7, Pin.OUT)
-a1_gruen = Pin(8, Pin.OUT)
+a1_red = Pin(6, Pin.OUT)
+a1_yellow = Pin(7, Pin.OUT)
+a1_green = Pin(8, Pin.OUT)
 
-a2_rot = Pin(18, Pin.OUT)
-a2_gelb = Pin(19, Pin.OUT)
-a2_gruen = Pin(20, Pin.OUT)
+a2_red = Pin(18, Pin.OUT)
+a2_yellow = Pin(19, Pin.OUT)
+a2_green = Pin(20, Pin.OUT)
 
 
+def red_to_green(traffic_light):
+    if traffic_light == 1:
+        a1_yellow.toggle()
+        sleep(2)
+        a1_red.off()
+        a1_yellow.off()
+        a1_green.on()
+    elif traffic_light == 2:
+        a2_yellow.toggle()
+        sleep(2)
+        a2_red.off()
+        a2_yellow.off()
+        a2_green.on()
+
+
+def green_to_red(traffic_light):
+    if traffic_light == 1:
+        a1_green.off()
+        a1_yellow.on()
+        sleep(2)
+        a1_red.on()
+        a1_yellow.off()
+    elif traffic_light == 2:
+        a2_green.off()
+        a2_yellow.on()
+        sleep(2)
+        a2_red.on()
+        a2_yellow.off()
+
+a1_red.on()
+a2_green.on()
 while True:
     try:
-        led.toggle()
-        a1_rot.toggle()
-        a1_gelb.toggle()
-        a1_gruen.toggle()
-        a2_rot.toggle()
-        a2_gelb.toggle()
-        a2_gruen.toggle()
-        sleep(2)
-        led.off()
-        a1_rot.off()
-        a1_gelb.off()
-        a1_gruen.off()
-        a2_rot.off()
-        a2_gelb.off()
-        a2_gruen.off()
-        sleep(2)
+        sleep(5)
+        green_to_red(2)
+        red_to_green(1)
+        sleep(5)
+        green_to_red(1)
+        red_to_green(2)
+        sleep(5)
     except KeyboardInterrupt:
         break
